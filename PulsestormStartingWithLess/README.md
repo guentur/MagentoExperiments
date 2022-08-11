0de79147e22c7bbc310d9a300a59873feabe134f
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-08-11 16:55:33 +0300

    Init new training module

---
+++ PulsestormStartingWithLess/README.md
@@ -0,0 +1,2 @@
+# Guentur_PulsestormStartingWithLess
+My training practise by [this article](https://alanstorm.com/magento_2_less_css/) titled Magento 2 and the Less CSS Preprocessor
---
+++ PulsestormStartingWithLess/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Guentur_PulsestormStartingWithLess',
+        __DIR__
+    );
