78125b5916be15828e8f4ccdb9ddf45a9fb1d9df
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-08-12 11:42:08 +0300

    add simple JavaScript component

--- PulsestormJavascriptInitTutorial/view/frontend/web/example.js
+++ PulsestormJavascriptInitTutorial/view/frontend/web/example.js
@@ -1,4 +1,8 @@
-define([], function(){
-    alert("A simple RequireJS module");
-    return {};
+define([], function () {
+    var mageJsComponent = function()
+    {
+        alert("A simple magento component.");
+    };
+
+    return mageJsComponent;
 });
