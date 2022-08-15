0f99062020171b7578d08437b103db6469899386
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-08-15 17:00:20 +0300

    Add simple mixin

---
+++ PulsestormRequireJsRewrite/view/base/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    'config':{
+        'mixins': {
+            'Magento_Customer/js/view/customer': {
+                'Guentur_PulsestormRequireJsRewrite/hook':true
+            }
+        }
+    }
+};
