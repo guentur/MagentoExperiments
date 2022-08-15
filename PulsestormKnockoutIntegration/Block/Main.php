d1ad544c4da1b267698861bf97005fdfb95e4e46
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-08-15 13:15:40 +0300

    Added route and view layers

---
+++ PulsestormKnockoutIntegration/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Guentur\PulsestormKnockoutIntegration\Block;
+
+/**
+ * Class Main
+ */
+class Main extends \Magento\Framework\View\Element\Template
+{
+    function _prepareLayout(){}
+}
